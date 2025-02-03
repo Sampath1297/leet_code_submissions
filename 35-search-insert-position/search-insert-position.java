a63ab0af6ae3cc6d36dc66546638980a2d3fbaa8
Sampath Polisetti <sampathpolisetti1297@gmail.com>
2025-02-03 12:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (19.05%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,18 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int len = nums.length;
-        int index = 0;
+       int left=0,right=nums.length-1;
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            return mid;
 
-        // No need to sort the array, it's already sorted
-        for (int i = 0; i < len; i++) {
-            if (nums[i] == target) {
-                return i;  // Target found, return the index
-            } else if (nums[i] > target) {
-                return i;  // Target should be inserted here
-            }
         }
-        
-        // If we reach here, the target is greater than all elements, insert at the end
-        return len;
+        else if(nums[mid]<target)
+        {
+            left=mid+1;
+        }
+        else{
+            right =mid-1;
+
+        }
+       }
+       return left ;
+       
     }
 }
